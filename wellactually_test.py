1c707c760326028ed8d94219e2854db2e30d00dd
mike stedman <github@ravuya.com>
2014-11-06 15:52:21 -0500

    added quick test case

---
+++ wellactually_test.py
@@ -0,0 +1,10 @@
+import wellactually as wa
+
+assert len(wa.wordcache) == 0, "Word cache should be empty at startup"
+
+# basic add
+wa.add_synonyms_to_cache('emit', ['eject', 'discharge'])
+assert len(wa.wordcache) == 3, "Three words should be produced when exposing three new words"
+assert len(wa.wordcache['emit']) == 2, "Two synonyms for 'emit' should have been recorded"
+
+print 'pass'
